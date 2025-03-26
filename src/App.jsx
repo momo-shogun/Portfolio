b6e22dc7840c791790d47bb1a8ef9e5d7cdbe7bf
momo_shogun <krishnafinger12345@gmail.com>
2025-03-26 17:42:49 +0530

    changed introhead

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <div className="px-4 py-2 mt-20 space-y-1 leading-relaxed sm:px-8 md:px-16 lg:px-24 xl:px-36 md:mt-24 lg:mt-28">
         <p className="text-xl font-semibold sm:text-2xl">Hi, I'm Krishna👋</p>
         <p className="text-3xl font-extrabold tracking-normal sm:text-4xl md:text-5xl">
-          I Build Full Stack Web Applications.
+        I build Full Stack Web Applications and Mobile Apps  
         </p>
       </div>
       <ProjectCarousel></ProjectCarousel>
